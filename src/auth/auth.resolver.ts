ef6e8c84f1c88630972afda5ef39ab3b80ee5c02
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-15 18:49:06 +0330

    clean auth service

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -16,25 +16,6 @@ export class AuthResolver {
   login(@Args('loginUserInput') loginUserInput: LoginUserInput) {
     return this.authService.login(loginUserInput);
   }
+  
 
-  @Query(() => [Auth], { name: 'auth' })
-
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Query(() => Auth, { name: 'auth' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.authService.findOne(id);
-  }
-
-  // @Mutation(() => Auth)
-  // updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
-  //   return this.authService.update(updateAuthInput.id, updateAuthInput);
-  // }
-
-  @Mutation(() => Auth)
-  removeAuth(@Args('id', { type: () => Int }) id: number) {
-    return this.authService.remove(id);
-  }
 }
