b15714bb24d45a622fe2f064bb9590b876a41c9f
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-15 18:35:39 +0330

    add configModule & userModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,15 +5,21 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { PrismaService } from './shared/prisma.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal:true
+    }),
   GraphQLModule.forRoot<ApolloDriverConfig>({
       driver:ApolloDriver,
       autoSchemaFile:join(process.cwd(),'src/schema.gql'),
       sortSchema:true
     }),
   AuthModule,
+  UserModule,
 
   ],
   
