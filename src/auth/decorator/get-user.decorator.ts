93938bd1410883d97febb3d8d580facb2eba4b48
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-15 18:37:16 +0330

    create user decorator

---
+++ src/auth/decorator/get-user.decorator.ts
@@ -0,0 +1,13 @@
+import { createParamDecorator } from "@nestjs/common/decorators";
+import { ExecutionContext } from "@nestjs/common/interfaces";
+import { User } from "@prisma/client";
+
+export const GetUSer = createParamDecorator(
+    (data: string | undefined, ctx: ExecutionContext): User => {
+        const request : Express.Request = ctx.switchToHttp().getRequest();
+        if(data){
+            return request.user[data];
+        }
+        return request.user as User;
+    },
+);
