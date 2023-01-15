b65fc43a65e762b67706100fe5ac49d81d6d1961
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-15 18:37:02 +0330

    create login service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthInput } from './dto/create-auth.input';
-import { UpdateAuthInput } from './dto/update-auth.input';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { JwtService } from 'src/shared/jwt.service';
+import { PrismaService } from './../shared/prisma.service';
+import { LoginUserInput } from './dto/login-user-input';
+
 
 @Injectable()
 export class AuthService {
-  create(createAuthInput: CreateAuthInput) {
-    return 'This action adds a new auth';
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) { }
+
+  async login(loginUserInput: LoginUserInput) {
+    const user = await this.prisma.user.findUnique({ where: { email: loginUserInput.email } });
+    if (!user)
+      throw new ForbiddenException("User Not Found");
+    if (user.password !== loginUserInput.password)
+      throw new ForbiddenException('Username or Password is entered incorrectly');
+    return{
+      access_token: this.jwtService.sign(user.id, user.email),
+      user:{...user}
   }
+}
+
+  // create(createAuthInput: CreateAuthInput) {
+  //   return 'This action adds a new auth';
+  // }
 
   findAll() {
     return `This action returns all auth`;
@@ -16,9 +35,9 @@ export class AuthService {
     return `This action returns a #${id} auth`;
   }
 
-  update(id: number, updateAuthInput: UpdateAuthInput) {
-    return `This action updates a #${id} auth`;
-  }
+  // update(id: number, updateAuthInput: UpdateAuthInput) {
+  //   return `This action updates a #${id} auth`;
+  // }
 
   remove(id: number) {
     return `This action removes a #${id} auth`;
