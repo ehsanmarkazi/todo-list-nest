1aa6100f7de79d449228480991db5583cda1fc50
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-15 18:36:46 +0330

    create login resolver

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -1,19 +1,24 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
 import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
-import { CreateAuthInput } from './dto/create-auth.input';
+import { LoginUserInput } from './dto/login-user-input';
 import { UpdateAuthInput } from './dto/update-auth.input';
+import { JwtGuard } from './guard/jwt.guard';
+import { UseGuards, Get } from '@nestjs/common';
+import { GetUSer } from './decorator/get-user.decorator';
+import { LoginResponse } from './dto/login-response.dto';
 
 @Resolver(() => Auth)
 export class AuthResolver {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
-  @Mutation(() => Auth)
-  createAuth(@Args('createAuthInput') createAuthInput: CreateAuthInput) {
-    return this.authService.create(createAuthInput);
+  @Mutation(() => LoginResponse)
+  login(@Args('loginUserInput') loginUserInput: LoginUserInput) {
+    return this.authService.login(loginUserInput);
   }
 
   @Query(() => [Auth], { name: 'auth' })
+
   findAll() {
     return this.authService.findAll();
   }
@@ -23,10 +28,10 @@ export class AuthResolver {
     return this.authService.findOne(id);
   }
 
-  @Mutation(() => Auth)
-  updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
-    return this.authService.update(updateAuthInput.id, updateAuthInput);
-  }
+  // @Mutation(() => Auth)
+  // updateAuth(@Args('updateAuthInput') updateAuthInput: UpdateAuthInput) {
+  //   return this.authService.update(updateAuthInput.id, updateAuthInput);
+  // }
 
   @Mutation(() => Auth)
   removeAuth(@Args('id', { type: () => Int }) id: number) {
