aa69642abc1e8afb15c1dc6cdf7c19723e695d0b
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-16 14:59:15 +0330

    remove old jwt

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -3,9 +3,6 @@ import { AuthService } from './auth.service';
 import { Auth } from './entities/auth.entity';
 import { LoginUserInput } from './dto/login-user-input';
 import { RegisterResponse } from './dto/register-response.dto';
-import { JwtGuard } from './guard/jwt.guard';
-import { UseGuards, Get } from '@nestjs/common';
-import { GetUser } from './decorator/get-user.decorator';
 import { LoginResponse } from './dto/login-response.dto';
 import { RegisterUserInput } from './dto/register-user-input.dto';
 
