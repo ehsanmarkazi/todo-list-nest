8183dc0f125c1e9e000283af30b9b9d9573f2853
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-16 14:59:26 +0330

    add guard

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -1,7 +1,13 @@
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { GqlExecutionContext } from '@nestjs/graphql';
 
-export class JwtGuard extends AuthGuard('jwt') {
-    constructor() {
-        super();
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+    getRequest(context: ExecutionContext) {
+        const ctx = GqlExecutionContext.create(context);
+        return ctx.getContext().req;
     }
 }
+
