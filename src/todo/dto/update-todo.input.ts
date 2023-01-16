d8c93be0b91ee316bced311a7dec775514eae8b5
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-16 19:09:00 +0330

    add enum to input & resolver

--- src/todo/dto/update-todo.input.ts
+++ src/todo/dto/update-todo.input.ts
@@ -1,8 +1,19 @@
 import { CreateTodoInput } from './create-todo.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { InputType, Field, PartialType, registerEnumType } from '@nestjs/graphql';
+
 
 @InputType()
 export class UpdateTodoInput extends PartialType(CreateTodoInput) {
-  @Field(() => Int)
-  id: number;
+  @Field()
+  id: string;
+  
+  @Field(() => AllowedStatus)
+  status: AllowedStatus;
 }
+export enum AllowedStatus {
+  todo = "To_do",
+  inProgress = "in_progress",
+  done = "done"
+}
+registerEnumType(AllowedStatus, { name: 'SomeEnum' })
+
