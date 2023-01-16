e1bb64fa5b5351fc6cc32162a1a8ae18964238f0
Ehsan Markazi <114006213+ehsanmarkazi@users.noreply.github.com>
2023-01-16 14:57:14 +0330

    add jwtStrategy in authModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { SharedModule } from 'src/shared/shared.module';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
-  providers: [AuthResolver, AuthService],
+  providers: [AuthResolver, AuthService, JwtStrategy],
   imports: [SharedModule],
 })
 export class AuthModule { }
